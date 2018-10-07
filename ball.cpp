1b4c5c56a7ac0399b962abf4a6be2cfdbfbfe4ba
ludovicm67 <ludovicmuller1@gmail.com>
2018-10-08 00:53:35 +0200

    fixed collisions on window sides

--- ball.cpp
+++ ball.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 
 #define BALL_SIZE 24
+#define PLAYER_HEIGHT 32
+#define PLAYER_WIDTH 128
 
 Ball::Ball()
     : m_ball({0, 0, BALL_SIZE, BALL_SIZE}), m_speedX(5), m_speedY(7),
@@ -28,34 +30,43 @@ void Ball::set_magnet() { m_is_moving = true; }
 void Ball::set_moving() { m_is_moving = true; }
 
 void Ball::move(Player &player) {
+  int playerPosition = player.get_x();
+
   if (m_is_moving) {
     // deplacement
     m_ball.x += m_speedX;
     m_ball.y += m_speedY;
 
-    // collision bord
-    if ((m_ball.x < 1) || (m_ball.x > m_windowX - 25)) {
+    if (m_ball.x < 1) { // collision bord gauche
       m_speedX *= -1;
-    }
-    if ((m_ball.y < 1) || (m_ball.y > m_windowY - 25)) {
+      if (m_ball.x < 0)
+        m_ball.x = 1;
+    } else if (m_ball.y < 1) { // collision bord haut
       m_speedY *= -1;
-    }
+      if (m_ball.y < 0)
+        m_ball.y = 1;
+    } else if (m_ball.x > m_windowX - BALL_SIZE) { // collision bord droit
+      m_ball.x = -m_ball.x + (2 * (m_windowX - BALL_SIZE));
+      m_speedX *= -1;
+    } else if (m_ball.y > m_windowY - BALL_SIZE) { // collsion bord bas
+      m_ball.y = -m_ball.y + (2 * (m_windowY - BALL_SIZE));
+      m_speedY *= -1;
 
-    // touche bas -> rouge
-    if (m_ball.y > (m_windowY - 25)) {
+      // touche bas -> rouge
       m_src.y = 64;
       player.loose_life();
     }
 
     // collision vaisseau
-    if ((m_ball.x > player.get_x()) && (m_ball.x < player.get_x() + 128) &&
-        (m_ball.y > m_windowY - 32 - 20)) {
+    if ((m_ball.x > playerPosition) &&
+        (m_ball.x < playerPosition + PLAYER_WIDTH) &&
+        (m_ball.y > m_windowY - PLAYER_HEIGHT - 20)) {
       m_speedY *= -1;
       m_src.y = 96; // -> vert
     }
 
   } else { // the ball sticks on the player
-    m_ball.x = player.get_x();
+    m_ball.x = playerPosition;
   }
 }
 
