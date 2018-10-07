3644a2bdddc910cf2273bd32ca92ed0d09bfc5b9
ludovicm67 <ludovicmuller1@gmail.com>
2018-10-07 22:48:58 +0200

    close #8: collisions are now working as expected

--- brick.cpp
+++ brick.cpp
@@ -16,27 +16,25 @@ Brick::Brick(int lives)
                {140, 0, BRICK_WIDTH, BRICK_HEIGHT}),
       m_lives(lives) {}
 Brick::Brick(int lives, int line, int col)
-    : Drawable({col * BRICK_WIDTH, line * BRICK_HEIGHT, 0, 0},
-               {140, 0, BRICK_WIDTH, BRICK_HEIGHT}),
+    : Drawable(
+          {col * BRICK_WIDTH, line * BRICK_HEIGHT, BRICK_WIDTH, BRICK_HEIGHT},
+          {140, 0, BRICK_WIDTH, BRICK_HEIGHT}),
       m_lives(lives) {}
 
 bool Brick::checkCollision(Ball &ball) {
   SDL_Rect bRect = ball.getRect();
 
-  if (!(bRect.x > (m_rect.x + m_rect.w) || (bRect.x + bRect.w) < m_rect.x ||
-        bRect.y > (m_rect.y + m_rect.h) || (bRect.y + bRect.h) < m_rect.y)) {
+  if (SDL_HasIntersection(&bRect, &m_rect)) {
     int deltaLeft = std::abs(bRect.x - m_rect.x);
     int deltaRight = std::abs(bRect.x + bRect.w - m_rect.x - m_rect.w);
     int deltaTop = std::abs(bRect.y - m_rect.y);
     int deltaBottom = std::abs(bRect.y + bRect.h - m_rect.y - m_rect.h);
 
-    if (deltaLeft < deltaTop && deltaLeft < deltaBottom)
+    if ((deltaLeft < deltaTop && deltaLeft < deltaBottom) ||
+        (deltaRight < deltaTop && deltaRight < deltaBottom))
       ball.collisionLeftRight();
-    else if (deltaRight < deltaTop && deltaRight < deltaBottom)
-      ball.collisionLeftRight();
-    else if (deltaTop < deltaLeft && deltaTop < deltaRight)
-      ball.collisionTopBottom();
-    else if (deltaBottom < deltaLeft && deltaBottom < deltaRight)
+    else if ((deltaTop < deltaLeft && deltaTop < deltaRight) ||
+             (deltaBottom < deltaLeft && deltaBottom < deltaRight))
       ball.collisionTopBottom();
 
     if (--m_lives < 1)
